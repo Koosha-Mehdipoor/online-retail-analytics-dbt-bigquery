ed3d4e2ef45907dca7e097a835e707574f70c3a2
Koosha <koosha.mehdipoor@gmail.com>
2026-01-27 17:32:28 +0100

    creating a view of the bronze table

---
+++ models/source/v_src_online_retail.sql
@@ -0,0 +1,21 @@
+
+{{config(
+    materialized = "view",
+    tags = ["source"]
+)}}
+
+SELECT 
+  trim(invoice_no) AS invoice_no,
+  trim(stock_code) AS stock_code,
+  coalesce(trim(description),"Not Defined") AS description,
+  CASE WHEN left(trim(invoice_no),1) = 'C' THEN "cancelled" ELSE "active" END AS order_status,
+  quantity,
+  EXTRACT (DATE FROM TIMESTAMP(safe.parse_datetime('%m/%d/%y %H:%M', trim(invoice_date)))) AS invoice_date,
+  EXTRACT (TIME FROM TIMESTAMP(safe.parse_datetime('%m/%d/%y %H:%M', trim(invoice_date)))) AS invoice_time,
+  unit_price,
+  coalesce(customer_id, "Not Registered") AS customer_id,
+  coalesce(country, "Unknown") AS country,
+  created_at
+FROM {{ref("src_online_retail")}}
+
+
