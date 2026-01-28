6bad655f4dc046d2b670924677788be7d5071012
Koosha <koosha.mehdipoor@gmail.com>
2026-01-28 15:27:00 +0100

    fact table creation

---
+++ models/facts/f_online_retail.sql
@@ -0,0 +1,21 @@
+{{ 
+    config(
+        materialized = 'table',
+        cluster_by = 'invoice_date_id',
+        tags = ['facts']
+    )
+}}
+
+SELECT 
+row_hash,
+invoice_date_id,
+invoice_no,
+stock_code,
+quantity,
+order_status,
+customer_id,
+country,
+CASE WHEN quantity > 0 THEN quantity * unit_price ELSE 0 END AS sales_amount,
+CASE WHEN quantity < 0 THEN quantity * unit_price ELSE 0 END AS return_amount,
+quantity*unit_price AS net_amount, 
+FROM {{ ref('stg_online_retail') }}
