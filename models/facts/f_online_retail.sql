a732b8daae739432001e1f32ece96ee3c68ca3ff
Koosha <koosha.mehdipoor@gmail.com>
2026-01-29 13:03:15 +0100

    change logic

--- models/facts/f_online_retail.sql
+++ models/facts/f_online_retail.sql
@@ -17,5 +17,5 @@ customer_id,
 country,
 CASE WHEN quantity > 0 THEN quantity * unit_price ELSE 0 END AS sales_amount,
 CASE WHEN quantity < 0 THEN quantity * unit_price ELSE 0 END AS return_amount,
-quantity*unit_price AS net_amount, 
+quantity*unit_price AS net_amount
 FROM {{ ref('stg_online_retail') }}
