d7870d7df606c42dcc09d62994fd780b7977c697
Koosha <koosha.mehdipoor@gmail.com>
2026-01-30 14:55:37 +0100

    name change

--- models/dimensions/dim_products.sql
+++ models/dimensions/dim_products.sql
@@ -15,7 +15,7 @@ SELECT
     unit_price,
     COUNT(DISTINCT invoice_no) AS total_orders,
     COUNT(DISTINCT CASE WHEN customer_id != 'Not Registered' THEN customer_id END)AS total_registered_customers,
-    COUNT(DISTINCT CASE WHEN customer_id = 'Not Registered' THEN invoice_no END) AS total_guest_orders,
+    COUNT(DISTINCT CASE WHEN customer_id = 'Not Registered' THEN invoice_no END) AS total_guest_customers,
     SUM(quantity) AS total_quantity_sold
 FROM {{ ref('stg_online_retail') }}
 GROUP BY
