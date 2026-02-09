ec69d974b9a629a787d11f0a676502b7c72aa9e1
Koosha <koosha.mehdipoor@gmail.com>
2026-02-09 12:44:40 +0100

    adding unique product display name

--- models/dimensions/dim_products.sql
+++ models/dimensions/dim_products.sql
@@ -21,6 +21,14 @@ FROM {{ ref('stg_online_retail') }}
      ROW_NUMBER() OVER(PARTITION BY stock_code ORDER BY CASE WHEN LOWER(`description`) = 'not defined' THEN 2 ELSE 1 END,description) AS rn
      FROM distinct_desc
 ),
+prod_display AS (
+    SELECT 
+        stock_code,
+        product_display,
+        ROW_NUMBER() OVER(PARTITION BY stock_code ORDER BY CASE WHEN LOWER(description) = 'not defined' THEN 1 ELSE 2 END DESC)  AS prn
+    FROM {{ ref('stg_online_retail') }}
+    WHERE product_display IS NOT NULL
+),
 dim_product AS (
     SELECT 
     UPPER(TRIM(stock_code)) AS stock_code,
@@ -34,8 +42,11 @@ FROM {{ ref('stg_online_retail') }}
 GROUP BY
     stock_code
 )
-SELECT dp.*, rd.description AS product_description FROM dim_product AS dp
+SELECT dp.stock_code,UPPER(TRIM(pd.product_display)) AS product_display, dp.first_sold_date, dp.last_sold_date, dp.total_orders, dp.total_registered_customers, dp.total_guest_customers, dp.total_quantity_sold,
+rd.description AS product_description FROM dim_product AS dp
 LEFT JOIN ranked AS rd
 ON dp.stock_code = rd.stock_code AND rd.rn = 1
+LEFT JOIN prod_display AS pd 
+ON dp.stock_code = pd.stock_code AND pd.prn = 1
 
 
